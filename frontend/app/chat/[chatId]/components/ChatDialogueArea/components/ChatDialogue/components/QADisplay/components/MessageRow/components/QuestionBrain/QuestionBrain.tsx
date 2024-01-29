b64c92271e150e98747e68939d70700a6eab054a
Stef Bloom <bloom-dev@pm.me>
2024-01-29 06:29:50 -0800

    am update

---
+++ frontend/app/chat/[chatId]/components/ChatDialogueArea/components/ChatDialogue/components/QADisplay/components/MessageRow/components/QuestionBrain/QuestionBrain.tsx
@@ -0,0 +1,23 @@
+import { Fragment } from "react";
+
+import Icon from "@/lib/components/ui/Icon/Icon";
+
+import styles from "./QuestionBrain.module.scss";
+
+type QuestionBrainProps = {
+  brainName?: string | null;
+};
+export const QuestionBrain = ({
+  brainName,
+}: QuestionBrainProps): JSX.Element => {
+  if (brainName === undefined || brainName === null) {
+    return <Fragment />;
+  }
+
+  return (
+    <div data-testid="brain-tags" className={styles.brain_name_wrapper}>
+      <Icon name="brain" color="black" size="normal" />
+      <span>{brainName}</span>
+    </div>
+  );
+};
