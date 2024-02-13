86899015053c6c9fc784734a82068e3033d5d8e7
Stef Bloom <bloom-dev@pm.me>
2024-02-13 09:22:38 -0800

    branding

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -15,7 +15,7 @@ const inter = Outfit({ subsets: ["latin"] });
 export const metadata = {
   title: "WildcatGPT - Get a Second Brain with Generative AI",
   description:
-    "Quivr is your second brain in the cloud, designed to easily store and retrieve unstructured information.",
+    "WildcatGPT is your second brain in the cloud, designed to easily store and retrieve unstructured information.",
 };
 
 const RootLayout = async ({
