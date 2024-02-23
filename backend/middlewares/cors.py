361b7934aec1262362a71e30429c809c1580f29b
Stef Bloom <bloom-dev@pm.me>
2024-02-23 06:12:41 -0800

    am update

--- backend/middlewares/cors.py
+++ backend/middlewares/cors.py
@@ -4,18 +4,10 @@ origins = [
     "http://localhost",
     "http://localhost:3000",
     "http://localhost:3001",
-    "http://localhost:5050",
-    "http://localhost:8000",
     "https://quivr.app",
     "https://www.quivr.app",
     "http://quivr.app",
     "http://www.quivr.app",
-    "http://wildcatgpt.com",
-    "http://www.wildcatgpt.com",
-    "https://wildcatgpt.com",
-    "https://www.wildcatgpt.com",
-    "http://dev.wildcatgpt.com",
-    "https://dev.wildcatgpt.com",
     "*",
 ]
 
