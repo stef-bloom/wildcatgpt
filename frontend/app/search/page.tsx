1372bc979ab1fcd391303ee436ac892a76dab5bf
Stef Bloom <bloom-dev@pm.me>
2024-02-26 06:29:35 -0800

    branding

--- frontend/app/search/page.tsx
+++ frontend/app/search/page.tsx
@@ -46,7 +46,7 @@ const Search = (): JSX.Element => {
           <div className={styles.quivr_logo_wrapper}>
             <QuivrLogo size={80} color="black" />
             <div className={styles.quivr_text}>
-              <span>Talk to </span>
+              <span>Chat with </span>
               <span className={styles.quivr_text_primary}>WildcatGPT</span>
             </div>
           </div>
