c51f8df75f117206343f150f3c4c6b1986578ee3
Stef Bloom <bloom-dev@pm.me>
2024-03-20 18:26:46 -0700

    am update

--- frontend/app/user/components/StripePricingOrManageButton.tsx
+++ frontend/app/user/components/StripePricingOrManageButton.tsx
@@ -1,34 +1,23 @@
+import { useTranslation } from "react-i18next";
+
 import { StripePricingModal } from "@/lib/components/Stripe";
-import QuivrButton from "@/lib/components/ui/QuivrButton/QuivrButton";
+import Button from "@/lib/components/ui/Button";
 import { useUserData } from "@/lib/hooks/useUserData";
 
 const MANAGE_PLAN_URL = process.env.NEXT_PUBLIC_STRIPE_MANAGE_PLAN_URL;
 
 export const StripePricingOrManageButton = (): JSX.Element => {
+  const { t } = useTranslation("monetization");
   const { userData } = useUserData();
 
   const is_premium = userData?.is_premium ?? false;
   if (is_premium) {
     return (
       <a href={MANAGE_PLAN_URL} target="_blank" rel="noopener">
-        <QuivrButton
-          label="Manage my plan"
-          color="gold"
-          iconName="star"
-        ></QuivrButton>
+        <Button className="w-full">{t("manage_plan")}</Button>
       </a>
     );
   }
 
-  return (
-    <StripePricingModal
-      Trigger={
-        <QuivrButton
-          label="Upgrade my plan"
-          color="gold"
-          iconName="star"
-        ></QuivrButton>
-      }
-    />
-  );
+  return <StripePricingModal Trigger={<Button>{t("upgrade")}</Button>} />;
 };
